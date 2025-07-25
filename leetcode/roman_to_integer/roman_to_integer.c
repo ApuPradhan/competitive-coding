2dd588ee3c210dc6ac10e148ee4627c30722a4d7
ApuPradhan <Apu.Pradhan@iskraemeco.in>
2025-07-25 16:10:03 +0530

    Problem: roman_to_integer

---
+++ leetcode/roman_to_integer/roman_to_integer.c
@@ -0,0 +1,44 @@
+int value(char ch) {
+    switch (ch) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char* s) {
+    int total = 0;
+    int i = 0;
+    
+    while (s[i] != '\0') {
+        int current = value(s[i]);
+        int next = value(s[i + 1]);
+
+        if (next > current) {
+            total += (next - current);
+            i += 2;  // skip the next character
+        } else {
+            total += current;
+            i++;
+        }
+    }
+
+    return total;
+}
+
+#include <stdio.h>
+
+int value(char ch);
+int romanToInt(char* s);
+
+int main() {
+    char s[] = "MCMXCIV";
+    printf("Roman numeral: %s\n", s);
+    printf("Integer: %d\n", romanToInt(s));
+    return 0;
+}
